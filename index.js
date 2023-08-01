b6180fd26b0ac04ad83168133b11505850566027
peter <pngugi.wainaina@gmail.com>
2023-08-02 00:10:20 +0300

    my arithmetic lab submission

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let multiply = 31*2;
+let num1=31;
+let num2=2;
+
+let random =0
+random ++
+
+let mod = 14 % 5
+let num3 =14
+let num4=5
+
+const max =Math.max(1, 2, 3, 4, 5,20);
